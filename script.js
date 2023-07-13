6573af7d7b6df254fb8622990079fd68ec2904c0
Mpho Matli <mphojames9@gmail.com>
2023-07-13 15:34:32 +0200

    update

--- script.js
+++ script.js
@@ -1,63 +1,48 @@
-let color = 'black';
+const resetBtn = document.querySelector("#reset");
+let inputValue = document.getElementById("value");
+let slideRanger = document.getElementById("sliderRange");
+let color = "black";
 let click = true;
-let reset = document.getElementById('reset');
-let screen = document.getElementById('screen');
 
-function populateBoard(size) {
-    let board = document.querySelector('.board');
-board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-let squares = document.querySelectorAll('div');
-squares.forEach((div)=> div.remove);
-
-let amount = size * size
-for(let i = 0; i < amount; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = ('white');
-    board.insertAdjacentElement('beforeend', square);
-    square.addEventListener('mouseover', colorSquare);
-};
-};
-
-populateBoard (16);
-
-function changeSize(input) {
-
-    if (input >=2 && input <=100) {
-        populateBoard(input);
-    }
-
-    else{
-        screen.innerText = 'Please select value between 2 and 100.';
-    }
-};
-
-function colorSquare(){
-    if(click) {
-        screen.innerText = 'Coloring Mode';
-        screen.style.backgroundColor = 'green';
-        if(color === "random") {
-            this.style.backgroundColor = `hsl(${Math.random()*360},100%, 50%)`;
-        }
-        else {this.style.backgroundColor = color}
-    }
-
-        else {
-            screen.innerText = 'Not coloring';
-            screen.style.backgroundColor = 'orange';
-        }
-
- 
-};
-
-function changeColor(choice) {
-    color = choice;
+changeSize(16)
+function changeSize(input){
+  populateBord(input)
+}
+function clickChecker(){
+    click = !click;
 }
 
-document.querySelector('body').addEventListener('click', function() {
-    click = !click;
-});
+function changeColor(choice){
+  color = choice;
+}
 
-reset.addEventListener('click', function(){
-    location.reload();
-})
+function populateBord(size){
+  const board = document.querySelector(".skatch");
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let amount = size * size;
+for(i = 0; i <= amount; i++){
+  let square = document.createElement("div");
+  square.style.background = "antiquewhite";
+  board.insertAdjacentElement("beforeend",square);
+  square.addEventListener("mouseover",colorSquare)
+
+function colorSquare(){
+ if (click){
+    if(color === "random"){
+
+    square.style.background = `hsl(${Math.random()*360},100%, 50%)`;
+  }else{
+    square.style.background = color;
+  }
+ }
+}
+}}
+slideRanger.oninput = function(){
+  inputValue.innerHTML = this.value;
+}
+
+if (screen.width >= '800px'){
+  arlet("error")
+}
